7d602e6ad7f7d3646206767d8993175561b84bfb
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-09-09 19:47:26 +0530

    commit

--- Firstclass.java
+++ Firstclass.java
@@ -1,5 +1,11 @@
 class Firstclass{
     public static void main(String  args []){
         System.out.println("Hello World");
+        for (int x=0; x<=3; x++){
+            for(int y=0;y<=x;y++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
