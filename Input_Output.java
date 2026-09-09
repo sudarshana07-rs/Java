a8117dbd2b43dafa86e9b4a3cacc94c5dde6fb4f
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-09-09 20:03:03 +0530

    commit

--- Input_Output.java
+++ Input_Output.java
@@ -1,6 +1,16 @@
 class Input_Output{
     public static void main(String agrs[]){
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.print("I am Sudarshana\n");
+        System.out.print("I am pursuing Engineering");
     }
 }
+
+
+
+
+// Output Statements --- 
+// 1) System.out.print("")
+// 2) System.out.println("")
+// 3)System.out.print("....\n")
 
