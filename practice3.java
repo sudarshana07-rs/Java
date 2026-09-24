4e1b45ea816614281bd7d6712a1150d26bfd7e0c
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-09-24 19:50:35 +0530

    commit

--- practice3.java
+++ practice3.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class practice3 {
-    public static 
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Welcome to XYZ supermart!!!");
+        System.out.println("Enter the no. of items: ");
+        int n= sc.nextInt();
+        String[] arr= new String[n];
+        for (int i=1; i<=n; i++){
+            System.out.println("Enter the item "+i+" :");
+            arr[i]= sc.nextLine();
+        }
+        for (int i=n; i<=n; i++){
+            System.out.println("Final Grocery List: \n");
+            System.out.println(arr[i]);
+        }
+    }
 }
