dd83cb60a3c0c9d304b28874dc4eb4c86c6ec812
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-09-24 19:52:25 +0530

    commit

--- practice3.java
+++ practice3.java
@@ -6,11 +6,12 @@ public class practice3 {
         System.out.println("Enter the no. of items: ");
         int n= sc.nextInt();
         String[] arr= new String[n];
+        // sc.next();
         for (int i=1; i<=n; i++){
             System.out.println("Enter the item "+i+" :");
-            arr[i]= sc.nextLine();
+            arr[i]= sc.next();
         }
-        for (int i=n; i<=n; i++){
+        for (int i=1; i<=n; i++){
             System.out.println("Final Grocery List: \n");
             System.out.println(arr[i]);
         }
