ede8c4828df929ffc40dbabda2230534a57d5d2b
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-09-25 19:42:54 +0530

    commit

--- practice4.java
+++ practice4.java
@@ -2,5 +2,13 @@ public class practice4 {
     public practice4(){
         float total;
         int quantity;
+        float price;
+        String item;
     }
+    
+    void gettotal(float price, int quantity){
+        price = price * quantity;
+    }
+
+    void calculateBill()
 }
